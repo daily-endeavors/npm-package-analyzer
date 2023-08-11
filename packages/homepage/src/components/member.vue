dffbf8164a2f19825f308d52be7b6e0c6990492e
wangke <854414767@qq.com>
2023-08-11 23:35:34 +0800

    Update member.vue

--- packages/homepage/src/components/member.vue
+++ packages/homepage/src/components/member.vue
@@ -14,54 +14,45 @@
         <tr>
           <td class="border border-slate-300">尧歌游</td>
           <td class="border border-slate-300">
-            <a href="https://github.com/yaogeyou-github" target="_blank"
-              >yaogeyou_github</a
-            >
+            <a href="https://github.com/yaogeyou-github" target="_blank">yaogeyou_github</a>
           </td>
-          <td
-            class="border border-slate-300 w-24 hover:animate-pulse flex justify-center content-center"
-          >
+          <td class="border border-slate-300 w-24 hover:animate-pulse flex justify-center content-center">
             <a href="https://github.com/yaogeyou-github" target="_blank">
-              <img
-                class="rounded-full w-16"
-                src="https://avatars.githubusercontent.com/u/141559074?s=400&u=839e2d7981c945b832d857d982996d314199eaee&v=4"
-              />
+              <img class="rounded-full w-16"
+                src="https://avatars.githubusercontent.com/u/141559074?s=400&u=839e2d7981c945b832d857d982996d314199eaee&v=4" />
             </a>
           </td>
         </tr>
         <tr>
           <td class="border border-slate-300">雪舟寺</td>
           <td class="border border-slate-300">
-            <a href="https://github.com/FionaYuliang" target="_blank"
-              >FionaYuliang_github</a
-            >
+            <a href="https://github.com/FionaYuliang" target="_blank">FionaYuliang_github</a>
           </td>
-          <td
-            class="border border-slate-300 w-24 hover:animate-pulse flex justify-center content-center"
-          >
+          <td class="border border-slate-300 w-24 hover:animate-pulse flex justify-center content-center">
             <a href="https://github.com/FionaYuliang" target="_blank">
-              <img
-                class="rounded-full w-16"
-                src="https://avatars.githubusercontent.com/u/20492174?v=4"
-              />
+              <img class="rounded-full w-16" src="https://avatars.githubusercontent.com/u/20492174?v=4" />
             </a>
           </td>
         </tr>
         <tr>
           <td class="border border-slate-300">戴戴戴yc</td>
           <td class="border border-slate-300">
-            <a href="https://github.com/2003dyc" target="_blank"
-              >DaiDaiDaiyc_github</a
-            >
+            <a href="https://github.com/2003dyc" target="_blank">DaiDaiDaiyc_github</a>
           </td>
-          <td
-            class="border border-slate-300 w-24 hover:animate-pulse flex justify-center content-center"
-          >
+          <td class="border border-slate-300 w-24 hover:animate-pulse flex justify-center content-center">
             <a href="https://github.com/2003dyc" target="_blank">
-              <img
-                class="rounded-full w-16"
-                src="https://avatars.githubusercontent.com/u/141700623?v=4"
-              />
+              <img class="rounded-full w-16" src="https://avatars.githubusercontent.com/u/141700623?v=4" />
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td class="border border-slate-300">Dynamite</td>
+          <td class="border border-slate-300">
+            <a href="https://github.com/jjiuen" target="_blank">jjiuen_github</a>
+          </td>
+          <td class="border border-slate-300 w-24 hover:animate-pulse flex justify-center content-center">
+            <a href="https://github.com/jjiuen" target="_blank">
+              <img class="rounded-full w-16" src="https://avatars.githubusercontent.com/u/122665661?v=4" />
             </a>
           </td>
         </tr>
@@ -74,6 +65,7 @@
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
+
 .read-the-docs {
   color: #888;
 }
