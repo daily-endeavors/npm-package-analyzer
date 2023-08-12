4996ca4e61a93777a69503ba411ad9c99d7e8bf4
FionaYuliang <yangmingming94@gmail.com>
2023-08-12 20:05:40 +0800

    feat:完成读取流程验证

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,14 +6,25 @@ import * as path from 'path'
 
 async function asyncRunner() {
     
-    const directoryPath = __dirname;
-    const fileName = 'example.txt'; // 新建文件名
+    // const directoryPath = __dirname;
+    // const fileName = 'example.txt'; // 新建文件名
     
-    const content = '这是一个示例文件的内容。'; // 文件内容
+    // const content = '这是一个示例文件的内容。'; // 文件内容
     
-    fs.writeFileSync(path.join(directoryPath, fileName), content);
+    // fs.writeFileSync(path.join(directoryPath, fileName), content);
 
-    console.log('done');
+
+    const rootPath = path.resolve(__dirname, '../../../')
+
+    const desfile = path.join(rootPath,'./docs/项目启动前对chatgpt的咨询记录.md')
+
+    const content = fs.readFileSync(desfile).toString()
+
+
+    console.log(content);
+
+
+    
 }
 
 
