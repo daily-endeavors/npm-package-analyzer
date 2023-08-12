1427ed721924ce015b107a1e8430817a3e831f0f
FionaYuliang <yangmingming94@gmail.com>
2023-08-12 19:49:55 +0800

    feart:完成写入流程验证

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,14 +1,24 @@
 import * as Const from './resource/const'
 import * as Type from './resource/type'
+import * as fs from 'fs'
+import * as path from 'path'
 
 
 async function asyncRunner() {
-    const demo: Type.json = {
-        "package_name": "demo",
-        "version": "3.4.5"
-    }
+    
+    const directoryPath = __dirname;
+    const fileName = 'example.txt'; // 新建文件名
+    
+    const content = '这是一个示例文件的内容。'; // 文件内容
+    
+    fs.writeFileSync(path.join(directoryPath, fileName), content);
 
-    console.log("hello world", "demo => ", demo)
+    console.log('done');
 }
+
+
+
+
+
 
 asyncRunner()
