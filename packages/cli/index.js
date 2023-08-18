e8603f03bc4ddaa40d084e5739a598c7329c01ca
FionaYuliang <yangmingming94@gmail.com>
2023-08-18 23:17:27 +0800

    feat: 修复 typo

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const runner = require('./build/src/index')
 console.log('开始执行')
-runner.asyncRunner().finaily(() => {
+runner.asyncRunner().finally(() => {
   console.log('执行完毕')
 })
