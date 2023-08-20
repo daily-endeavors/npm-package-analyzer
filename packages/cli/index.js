b4b61b2516b17c17fb1ece1d858875f98d8c5116
FionaYuliang <yangmingming94@gmail.com>
2023-08-20 22:57:03 +0800

    feat(cli): 尝试修复 esm 下无法运行的问题

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-const currentPkg = require('./package.json')
+import currentPkg from './package.json'
+import runner from './build/src/index'
 console.log(`当前运行指令: ${currentPkg.name}@${currentPkg.version}`)
 console.log(``)
-const runner = require('./build/src/index')
 console.log('开始执行')
 runner.asyncRunner().finally(() => {
   console.log('执行完毕')
